2a708723a16ba4c948225060cdf682964a31ad3d
KishorCK148 <104266492+KishorCK148@users.noreply.github.com>
2022-04-23 20:56:25 +0530

    Week1 files (#2)

---
+++ WEEK 1/Assignment_3/assignment3.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#take the input of csv file name which is passed as an argument
+
+
+#print the details of only 1st and 3rd column, if the first column data starts with letter "S"
+#and sort it alphabetically based on first column data
+
---
+++ WEEK 1/Assignment_4/assignment4.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#get the Cartesian x and y coodinates of a point that are passed as arguments
+
+
+#convert the Cartesian coordinates to the corresponding polar coordinates
+#compute radius and theta (in degrees) upto 5 decimal places
+#note: theta should range from 0 to 360 degrees
+
