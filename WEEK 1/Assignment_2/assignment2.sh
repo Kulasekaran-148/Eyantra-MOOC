6c5a42c17c1e99813d539026921088511b82e71c
KishorCK148 <104266492+KishorCK148@users.noreply.github.com>
2022-04-23 20:55:21 +0530

    Week1 files

---
+++ WEEK 1/Assignment_2/assignment2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#take the input of csv file name which is passed as an argument
+
+
+#print complete details only if the first column data is starting with letter "R"
+#and sort it alphabetically based on first column data
+
